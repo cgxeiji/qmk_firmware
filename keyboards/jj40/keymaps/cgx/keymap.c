1396da326cabaca6c99f0499e859423c01300dd5
Eiji Onchi <onchi.eiji@gmail.com>
2018-07-05 11:14:28 +0900

    Default layout

--- keyboards/jj40/keymaps/cgx/keymap.c
+++ keyboards/jj40/keymaps/cgx/keymap.c
@@ -168,9 +168,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Symbol
  * ,-----------------------------------------------------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  | CGx  |QWERTY|      |   (  |   )  |      |      |
+ * |      |  F1  |  F2  |  F3  |  F4  |      |      |      |   (  |   )  |      | CGx  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F5  |  F6  |  F7  |  F8  |      |      |      |   [  |   ]  |      |      |
+ * |      |  F5  |  F6  |  F7  |  F8  |      |      |      |   [  |   ]  |      |QWERTY|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F9  |  F10 |  F11 |  F12 |      |      |      |   {  |   }  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -178,8 +178,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_SYM] = KEYMAP( \
-  _______, KC_F1, KC_F2,  KC_F3,  KC_F4,  CGX, QWERTY, _______, KC_LPRN, KC_RPRN, _______, _______, \
-  _______, KC_F5, KC_F6,  KC_F7,  KC_F8,  _______,  _______,     _______, KC_LBRC, KC_RBRC, _______, _______, \
+  _______, KC_F1, KC_F2,  KC_F3,  KC_F4, _______, _______, _______, KC_LPRN, KC_RPRN, _______, CGX, \
+  _______, KC_F5, KC_F6,  KC_F7,  KC_F8,  _______,  _______,     _______, KC_LBRC, KC_RBRC, _______, QWERTY, \
   _______, KC_F9, KC_F10, KC_F11, KC_F12, _______,  _______,     _______, KC_LCBR, KC_RCBR, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
@@ -210,13 +210,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Win  | Alt  |Symbol|Shift |Mouse |Number|Space | Del  | VolD | VolU |  Esc |
+ * | Ctrl | Win  | Alt  |Symbol|Space |Mouse |Number|Space | Del  | VolD | VolU |  Esc |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_BSPC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_LCTL, KC_LGUI, KC_LALT, TT(_SYM), KC_LSFT,  TT(_MOUSE),  TT(_NUM),  KC_SPC,   KC_DEL, KC_VOLD, KC_VOLU, KC_ESC \
+  KC_LCTL, KC_LGUI, KC_LALT, TT(_SYM), KC_SPC,  TT(_MOUSE),  TT(_NUM),  KC_SPC,   KC_DEL, KC_VOLD, KC_VOLU, KC_ESC \
 )
 };
