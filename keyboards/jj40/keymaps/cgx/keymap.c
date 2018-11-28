a9939b6da55e41d768587a37f0b1ec004b51de23
Eiji Onchi <onchi.eiji@gmail.com>
2018-11-28 16:28:27 +0900

    refactoring

--- keyboards/jj40/keymaps/cgx/keymap.c
+++ keyboards/jj40/keymaps/cgx/keymap.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "jj40.h"
+#include QMK_KEYBOARD_H
 #include "action_layer.h"
 #include "timer.h"
 #include "eeconfig.h"
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 
-  [_CGX] = KEYMAP( \
+  [_CGX] = LAYOUT_ortho_4x12( \
   KC_TAB  , KC_W   , KC_L  , KC_R  , KC_B     , KC_GRV    , KC_PIPE , KC_Y  , KC_I   , KC_U   , KC_V   , KC_QUOT, \
   KC_LCTRL, KC_S   , KC_N  , KC_T  , KC_D     , KC_COMM   , KC_DOT  , KC_A  , KC_E   , KC_O   , KC_H   , KC_SCLN, \
   KC_LALT , KC_J   , KC_P  , KC_C  , KC_K     , KC_Q      , KC_Z    , KC_M  , KC_F   , KC_G   , KC_X   , KC_ENT , \
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_SHIFT] = KEYMAP( \
+[_SHIFT] = LAYOUT_ortho_4x12( \
   _______, _______, _______, _______, _______, _______, KC_QUES, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, KC_AMPR, KC_ASTR, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_NUM] = KEYMAP( \
+[_NUM] = LAYOUT_ortho_4x12( \
   _______, _______, KC_LABK, KC_RABK, _______, _______, _______,    KC_7,    KC_8,    KC_9, KC_MINS, _______,\
   _______, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, _______, _______,    KC_4,    KC_5,    KC_6,    KC_0, KC_PERC,\
   _______, KC_BSLS, KC_TILD, KC_CIRC,  KC_EQL, _______, _______,    KC_1,    KC_2,    KC_3, KC_SLSH, _______,\
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_NUM_SHIFT] = KEYMAP( \
+[_NUM_SHIFT] = LAYOUT_ortho_4x12( \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, \
@@ -178,7 +178,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_SYM] = KEYMAP( \
+[_SYM] = LAYOUT_ortho_4x12( \
   _______, KC_F1, KC_F2,  KC_F3,  KC_F4, _______, _______, _______, _______, _______, _______, CGX, \
   _______, KC_F5, KC_F6,  KC_F7,  KC_F8,  _______,  _______,     _______, _______, _______, _______, QWERTY, \
   _______, KC_F9, KC_F10, KC_F11, KC_F12, _______,  _______,     _______, _______, _______, _______, _______, \
@@ -196,7 +196,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_MOUSE] = KEYMAP( \
+[_MOUSE] = LAYOUT_ortho_4x12( \
   _______, KC_PGUP   , KC_HOME   , KC_UP      ,   KC_END  , _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, \
   _______, KC_PGDN   , KC_LEFT   , KC_DOWN    , KC_RIGHT  , _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, \
   _______, LCTL(KC_Z), LCTL(KC_X),  LCTL(KC_C), LCTL(KC_V), KC_CAPS, _______, KC_WH_L, KC_BTN3, KC_WH_R, _______, _______, \
@@ -214,7 +214,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl | Win  | Alt  |Symbol|Space |Mouse |Number|Space | Del  | VolD | VolU |  Esc |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = KEYMAP( \
+[_QWERTY] = LAYOUT_ortho_4x12( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_BSPC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
