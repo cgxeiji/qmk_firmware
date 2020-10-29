1234363822652663977af14fc10847ce09d7f519
Eiji Onchi <onchi.eiji@gmail.com>
2020-10-29 15:19:37 +0900

    Streamlined WPM.

--- keyboards/crkbd/keymaps/cgx/oled/utils.c
+++ keyboards/crkbd/keymaps/cgx/oled/utils.c
@@ -9,4 +9,12 @@ void shift_array(char a[][6], uint8_t idx, char data[], uint8_t max) {
   strcpy(a[idx], data);
 }
 
+void shift_uint8_array(uint8_t a[], uint8_t idx, uint8_t data, uint8_t max) {
+  if (idx >= max) {
+    return;
+  }
+  shift_uint8_array(a, idx+1, a[idx], max);
+  a[idx] = data;
+}
+
 #endif //CGX_UTILS
--- keyboards/crkbd/keymaps/cgx/oled/wpm.c
+++ keyboards/crkbd/keymaps/cgx/oled/wpm.c
@@ -1,56 +1,73 @@
 #include QMK_KEYBOARD_H
 #include "./utils.c"
 
-#define MAX_FRAMES 14
+#define Y_INIT (2*OLED_FONT_HEIGHT+4)
 
-char frames[MAX_FRAMES][6];
+// For horizontal displays, use OLED_DISPLAY_HEIGHT
+#define WPM_FRAMES (OLED_DISPLAY_WIDTH-Y_INIT-3)
+
+#define WPM_IMG_SIZE 32
+#define WPM_IMG_FRAMES 17
+
+uint8_t frames[WPM_FRAMES+1];
 uint16_t wpm_timer = 0;
 char buf[6];
 
-void update_frame(uint8_t val) {
-  char data[6] = "     \0";
+#define WPM_MIN 40
+#define WPM_MAX 100
 
-  if (val > 20) {
-    if (val < 30) {
-      data[2] = ':';
-    } else if (val < 40) {
-      data[2] = '.';
-    } else if (val < 50) {
-      data[1] = ':';
-      data[2] = '.';
-      data[3] = ':';
-    } else if (val < 60) {
-      data[1] = '.';
-      data[2] = 'o';
-      data[3] = '.';
-    } else if (val < 70) {
-      strcpy(data, ":.o.:\0");
-    } else if (val < 80) {
-      strcpy(data, ".oOo.\0");
-    } else if (val < 90) {
-      strcpy(data, "oO0Oo\0");
-    } else if (val < 100) {
-      strcpy(data, "o000o\0");
-    } else if (val < 110) {
-      strcpy(data, "O000O\0");
+void update_frame(uint8_t val) {
+  uint8_t frame = 0;
+  if (val > WPM_MIN) {
+    if (val > WPM_MAX) {
+      frame = WPM_IMG_FRAMES-1;
     } else {
-      strcpy(data, "00000\0");
+      frame = (val-WPM_MIN)*(WPM_IMG_FRAMES - 1)/(WPM_MAX - WPM_MIN);
     }
   }
-  shift_array(frames, 0, data, MAX_FRAMES);
+  shift_uint8_array(frames, 0, frame, WPM_FRAMES+1);
 }
+
+bool frame[WPM_IMG_SIZE];
+
+bool * frame_builder(uint8_t val) {
+  uint8_t from = WPM_IMG_SIZE / 2 - val;
+
+  for (uint8_t i = 0; i < WPM_IMG_SIZE/2; i++) {
+    frame[i] = (i >= from);
+    frame[WPM_IMG_SIZE-1-i] = frame[i];
+  }
+
+  return frame;
+}
+
 
 void render_wpm(void) {
   oled_write_P(PSTR("WPM: "), false);
   uint8_t current_wpm = get_current_wpm();
-  sprintf(buf, "%5d", current_wpm);
-  oled_write(buf, false);
-  if (timer_elapsed(wpm_timer) > 500) {
-    update_frame(current_wpm);
-    wpm_timer = timer_read();
+  for (uint8_t x = 0; x < 32; x++) {
+    oled_write_pixel(x, Y_INIT-3, true);
   }
 
-  for (uint8_t i = 0; i < MAX_FRAMES; i++) {
-    oled_write(frames[i], false);
+  sprintf(buf, "%5d", current_wpm);
+  oled_write(buf, false);
+
+  if (timer_elapsed(wpm_timer) > 50) {
+    update_frame(current_wpm);
+    wpm_timer = timer_read();
+
+    for (uint8_t y = 0; y < WPM_FRAMES; y++) {
+      if (frames[y] != frames[y+1]) { // skip drawing the same shape
+        frame_builder(frames[y]);
+        for (uint8_t x = 0; x < 32; x++) {
+          oled_write_pixel(x, y+Y_INIT, frame[x]);
+        }
+      }
+    }
+
+  }
+
+  for (uint8_t x = 0; x < 32; x++) {
+    oled_write_pixel(x, WPM_FRAMES+Y_INIT+2, true);
   }
 }
